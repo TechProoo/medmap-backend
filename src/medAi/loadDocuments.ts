dc6b4172d118d6b9ffaf883d61a837075c26591d
Oyetibo Olaoluwa <olaoluwaoyetibo@gmail.com>
2025-04-17 17:13:46 +0100

    Created a split function to split the documents into chunks

--- src/medAi/loadDocuments.ts
+++ src/medAi/loadDocuments.ts
@@ -2,12 +2,12 @@ import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
 
 import { CSVLoader } from "@langchain/community/document_loaders/fs/csv";
 
-const loadDocuments = async () => {
+export const loadDocuments = async () => {
   const loader = new DirectoryLoader("scripts/data", {
     ".csv": (path) => new CSVLoader(path),
   });
   const docs = await loader.load();
-  console.log({ docs });
+  // console.log({ docs });
 
   return docs;
 };

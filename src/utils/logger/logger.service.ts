bd9ad5a296426e195859338d9fc1d90f616f78fd
ProsperCoded <enweremproper@gmail.com>
2025-04-18 13:52:22 +0100

    🔧 Refactor LoggerService constructor to include outFile parameter - Added outFile parameter to control file logging behavior - Moved file path and existence checks inside the outFile condition - Simplified transport configuration for better readability

--- src/utils/logger/logger.service.ts
+++ src/utils/logger/logger.service.ts
@@ -7,41 +7,40 @@ import fs from "fs";
 import path from "path";
 export class LoggerService implements ILogger {
   private logger: pino.Logger;
-  constructor(private loggerPath: LoggerPaths) {
+  constructor(private loggerPath: LoggerPaths, outFile: boolean = false) {
     const label =
       Object.keys(LoggerPaths)[Object.values(LoggerPaths).indexOf(loggerPath)];
 
-    const fullPath = path.join(__dirname, "../../..", loggerPath);
+    if (outFile) {
+      const fullPath = path.join(__dirname, "../../..", loggerPath);
 
-    const dirExists = fs.existsSync(path.dirname(fullPath));
-    if (!dirExists) {
-      fs.mkdirSync(path.dirname(fullPath), { recursive: true });
-    }
-    const fileExists = fs.existsSync(fullPath);
+      const dirExists = fs.existsSync(path.dirname(fullPath));
+      if (!dirExists) {
+        fs.mkdirSync(path.dirname(fullPath), { recursive: true });
+      }
+      const fileExists = fs.existsSync(fullPath);
 
-    if (!fileExists) {
-      fs.writeFileSync(fullPath, "");
+      if (!fileExists) {
+        fs.writeFileSync(fullPath, "");
+      }
     }
-    // ! development mode enforced temporarily
-    const isDev = true || configService.get(ENV.NODE_ENV) === "development";
     const transport = pino.transport({
       targets: [
         // Pretty logs for console in development
-        isDev
+        {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+            translateTime: "yyyy-mm-dd HH:MM:ss",
+            ignore: "pid,hostname",
+          },
+        },
+        outFile
           ? {
-              target: "pino-pretty",
-              options: {
-                colorize: true,
-                translateTime: "yyyy-mm-dd HH:MM:ss",
-                ignore: "pid,hostname",
-              },
+              target: "pino/file",
+              options: { destination: this.loggerPath },
             }
           : null,
-        // File logging in JSON format
-        {
-          target: "pino/file",
-          options: { destination: this.loggerPath },
-        },
       ].filter(Boolean) as any[], // Remove null if not in development
     });
     this.logger = pino(

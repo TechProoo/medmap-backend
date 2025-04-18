da74e4c34012fb939073494250e97140352a040d
ProsperCoded <enweremproper@gmail.com>
2025-04-18 13:43:36 +0100

    🔧 Update package.json and Prisma schema
    - Updated the main entry point in package.json to point to the correct file.
    - Refactored build scripts in package.json for better clarity.
    - Removed unnecessary migration files and commented out the documents model in schema.prisma.
    - Added new migration for altering Drug, Pharmacy, and User tables.

--- prisma/migrations/20250418123810_removed_documents_table/migration.sql
+++ prisma/migrations/20250418123810_removed_documents_table/migration.sql
@@ -1,11 +1,20 @@
 /*
   Warnings:
 
+  - You are about to drop the column `inStock` on the `Drug` table. All the data in the column will be lost.
+  - Added the required column `updatedAt` to the `Drug` table without a default value. This is not possible if the table is not empty.
   - Added the required column `password` to the `Pharmacy` table without a default value. This is not possible if the table is not empty.
   - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
   - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
 
 */
+-- AlterTable
+ALTER TABLE "Drug" DROP COLUMN "inStock",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "imageUrl" TEXT,
+ADD COLUMN     "stocks" INTEGER NOT NULL DEFAULT 1,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
 -- AlterTable
 ALTER TABLE "Pharmacy" ADD COLUMN     "password" TEXT NOT NULL;
 
@@ -13,16 +22,6 @@ ALTER TABLE "Pharmacy" ADD COLUMN     "password" TEXT NOT NULL;
 ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 ADD COLUMN     "password" TEXT NOT NULL,
 ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
-
--- CreateTable
-CREATE TABLE "documents" (
-    "id" TEXT NOT NULL,
-    "content" TEXT NOT NULL,
-    "metadata" JSONB NOT NULL,
-    "embedding" vector,
-
-    CONSTRAINT "documents_pkey" PRIMARY KEY ("id")
-);
 
 -- CreateIndex
 CREATE INDEX "User_email_idx" ON "User"("email");

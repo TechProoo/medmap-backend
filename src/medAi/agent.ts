74ebbf1447078c9bf6404b14124939dbe78197c2
Oyetibo Olaoluwa <olaoluwaoyetibo@gmail.com>
2025-04-22 06:09:48 +0100

    Cors configuration

--- src/medAi/agent.ts
+++ src/medAi/agent.ts
@@ -15,7 +15,7 @@ const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173", // Your frontend URL
     methods: ["GET", "POST"],
-    credentials: true,
+    // credentials: true,
   },
 });
 
